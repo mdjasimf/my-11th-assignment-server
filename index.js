c18fa3288950b655aaba268956425853c9ece173
mdjasimf <mdjasimf@gmail.com>
2022-04-28 21:35:16 +0600

    create fruits server

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qgwct.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('books warehouse is running');
+
+})
+
+app.listen(port, () => {
+    console.log('books warehouse is running');
+})
