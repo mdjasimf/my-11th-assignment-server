4ace657ae01779006fd08e2b3bbabc2c36cef8fe
mdjasimf <mdjasimf@gmail.com>
2022-04-29 10:38:32 +0600

    id load

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 const app = express();
 
 app.use(cors());
@@ -9,9 +10,32 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qgwct.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://FRUITS:YIJprcW3zmldSMJZ@cluster0.qgwct.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+async function run() {
+    try {
+
+        await client.connect();
+        const AllFruitsCollection = client.db('fruitswarehouse').collection('fruits');
+        app.get('/allFruits', async (req, res) => {
+            const query = {};
+            const cursor = AllFruitsCollection.find(query);
+            const fruits = await cursor.toArray();
+            res.send(fruits);
+
+        });
+        app.get('/allFruits/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const fruits = await AllFruitsCollection.findOne(query);
+            res.send(fruits);
+        })
+    }
+    finally { }
+}
+run().catch(console.dir);
 
 
 
